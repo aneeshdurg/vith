13967bb3712eaef047f6b74d35a64ecbd264b8d1
Aneesh Durg <aneeshdurg17@gmail.com>
2021-02-08 19:50:43 -0800

    Add destructive drawing

--- modules/superformula.frag.c
+++ modules/superformula.frag.c
@@ -6,6 +6,8 @@ uniform float u_sf_m; /// { "start": 1, "end": 10, "default": 1 }
 uniform vec3 u_sf_n; /// { "start": [0, 0, 0], "end": [20, 20, 20], "default": [20,20,20], "names": ["n1", "n2", "n3"] }
 uniform float u_sf_thickness; /// { "start": 0, "end": 1, "default": 0.5 }
 uniform bool u_sf_smooth_edges; /// { "default": true }
+uniform bool u_sf_fill; /// { "default": false }
+uniform bool u_sf_destructive; /// { "default": false }
 
 void superformula() {
     vec2 coords = t_coords.xy;
@@ -27,12 +29,15 @@ void superformula() {
         pow(abs(sin(u_sf_m * theta / 4.)/b), u_sf_n.z),
         -1./u_sf_n.x);
 
-    if (abs(r - super_r) < u_sf_thickness) {
+    if (abs(r - super_r) < u_sf_thickness || (u_sf_fill && r < super_r)) {
         float factor = 1.;
-        if (u_sf_smooth_edges) {
+        if (u_sf_smooth_edges && (!u_sf_fill || r >= super_r)) {
             factor = 1. - abs(r - super_r) / u_sf_thickness;
         }
 
-        color_out.rgb += factor * u_sf_color;
+        if (u_sf_destructive && factor > 0.)
+            color_out.rgb = factor * u_sf_color;
+        else
+            color_out.rgb += factor * u_sf_color;
     }
 }
